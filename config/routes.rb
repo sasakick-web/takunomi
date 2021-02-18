e305b8a531a1f98099ddeb5d58a4a4cf98b44a72
takuya sasaki <sasakick12345@gmail.com>
2021-02-18 13:25:17 +0900

    ゲストログイン追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :likes, only: [:create, :destroy]
   root "posts#index"
+  devise_scope :user do
+     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+   end
   # post '/posts/guest_sign_in', to: 'posts#new_guest'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
